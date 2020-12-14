4c99a43bcf25a8603bf598e0aed030147b4d9097
reine93 <renata.colak@gmail.com>
2020-12-14 21:54:22 +0100

    Added comparison functions

--- app.js
+++ app.js
@@ -12,16 +12,16 @@
        
         // Create Dino Objects
      
-      const dino1 = new Dino ("Triceraptops", 5897, 340, "herbivore", "North America", "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh");
-      const dino2 = new Dino ("Tyrannosaurus Rex", 2450, 365, "carnivore", "North America", "Late Cretaceous", "The largest known skull measures in at 5 feet long.");
-      const dino3 = new Dino ("Anklyosaurus", 4762, 140, "herbivore", "North America", "Late Cretaceous", "Anklyosaurus survived for approximately 135 million years.");
-      const dino4 = new Dino ("Brachiosaurus", 31752, 945, "herbivore", "North America", "Late Jurasic", "An asteroid was named 9954 Brachiosaurus in 1991.");
-      const dino5 = new Dino ("Stegosaurus", 5262, 200, "herbivore", "North America", "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines.");
-      const dino6 = new Dino ("Elasmosaurus", 7258, 150, "carnivore", "North America", "Late Cretaceous", "Elasmosaurus was a marine reptile first discovered in Kansas.");
-      const dino7 = new Dino ("Pteranodon", 20, 51, "carnivore", "North America", "Late Cretaceous", "Actually a flying reptile, the Pteranodon is not a dinosaur.");
-      const pigeon = new Dino ("Pigeon", 0.5, 0.22, "herbivore", "World Wide", "Holocene", "all birds are living dinosaurs")
+      const dino1 = new Dino ("Triceraptops", 5897, 340, "Herbivore", "North America", "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh");
+      const dino2 = new Dino ("Tyrannosaurus Rex", 2450, 365, "Carnivore", "North America", "Late Cretaceous", "The largest known skull measures in at 5 feet long.");
+      const dino3 = new Dino ("Anklyosaurus", 4762, 140, "Herbivore", "North America", "Late Cretaceous", "Anklyosaurus survived for approximately 135 million years.");
+      const dino4 = new Dino ("Brachiosaurus", 31752, 945, "Herbivore", "North America", "Late Jurasic", "An asteroid was named 9954 Brachiosaurus in 1991.");
+      const dino5 = new Dino ("Stegosaurus", 5262, 200, "Herbivore", "North America", "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines.");
+      const dino6 = new Dino ("Elasmosaurus", 7258, 150, "Carnivore", "North America", "Late Cretaceous", "Elasmosaurus was a marine reptile first discovered in Kansas.");
+      const dino7 = new Dino ("Pteranodon", 20, 51, "Carnivore", "North America", "Late Cretaceous", "Actually a flying reptile, the Pteranodon is not a dinosaur.");
+      const pigeon = new Dino ("Pigeon", 0.5, 0.22, "Herbivore", "World Wide", "Holocene", "all birds are living dinosaurs")
 
-      let dinos = [dino1, dino2, dino3, dino4, dino5, dino6, dino7, pigeon]
+      let dinos = [dino1, dino2, dino3, dino4, dino5, dino6, dino7]
    
         // Create Human Object
 
@@ -52,25 +52,47 @@
         // Create Dino Compare Method 1
         // NOTE: Weight in JSON file is in lbs, height in inches. 
 
-    
+    function weightCompare () {
+     let randomDino = dinos[Math.floor(Math.random() * dinos.length)];
+     let weightRatio = (randomDino.weight / human.weight).toFixed(1);
 
-   
-    
+     if (weightRatio > 1) {
+     console.log (`${randomDino.species} weights ${weightRatio} times more than you!`) } 
+     else if (weightRatio < 1) {
+     console.log (`${randomDino.species} weights ${weightRatio} times less than you!`)
+     } else {`Your weight is same as ${randomDino.species} ` }
+    }
+
         
         // Create Dino Compare Method 2
-        // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    //weight compare
+    function heightCompare () {
+        let randomDino = dinos[Math.floor(Math.random() * dinos.length)];
+        let heightRatio = (randomDino.height / human.height).toFixed(1);
+    
+        if (heightRatio > 1) {
+        console.log (`${randomDino.species} is ${heightRatio} times taller than you!`) } 
+        else if (weightRatio < 1) {
+        console.log (`${randomDino.species} is ${heightRatio} times shorter than you!`)
+        } else {`Your height is same asw ${randomDino.species} ` }  //maybe not height ratio but difference?
+    }
 
-    //function CompareWeight
     
         
         // Create Dino Compare Method 3
-        // NOTE: Weight in JSON file is in lbs, height in inches.
-
     //diet compare
+     
+    function dietCompare() {
+      let randomDino = dinos[Math.floor(Math.random() * dinos.length)];
 
-    //function CompareDiet
+      if (randomDino.diet == human.diet) {
+          console.log (`${randomDino.species} is also a ${human.diet} just like you!`)
+      } else {console.log (`${randomDino.species}\'s diet is different than yours. He is a ${randomDino.diet}`)}
+    };
+
+
+
+
     
     
         // Generate Tiles for each Dino in Array
