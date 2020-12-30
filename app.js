f3c253fe210411bee2a24e3b96b70a00f45c7663
reine93 <renata.colak@gmail.com>
2020-12-30 22:23:53 +0100

    finally managed to make shuffled dinosaur tiles with human in the center

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
     // Create Dino Constructor
     function Dino (name, kg, cm, food, where, when, truth) {
-        this.name = name
+        this.species = name
         this.weight = kg
         this.height = cm
         this.diet = food
@@ -11,47 +11,107 @@
     };
        
         // Create Dino Objects
-     
-      const dino1 = new Dino ("Triceraptops", 5897, 340, "Herbivore", "North America", "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh");
-      const dino2 = new Dino ("Tyrannosaurus Rex", 2450, 365, "Carnivore", "North America", "Late Cretaceous", "The largest known skull measures in at 5 feet long.");
-      const dino3 = new Dino ("Anklyosaurus", 4762, 140, "Herbivore", "North America", "Late Cretaceous", "Anklyosaurus survived for approximately 135 million years.");
-      const dino4 = new Dino ("Brachiosaurus", 31752, 945, "Herbivore", "North America", "Late Jurasic", "An asteroid was named 9954 Brachiosaurus in 1991.");
-      const dino5 = new Dino ("Stegosaurus", 5262, 200, "Herbivore", "North America", "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines.");
-      const dino6 = new Dino ("Elasmosaurus", 7258, 150, "Carnivore", "North America", "Late Cretaceous", "Elasmosaurus was a marine reptile first discovered in Kansas.");
-      const dino7 = new Dino ("Pteranodon", 20, 51, "Carnivore", "North America", "Late Cretaceous", "Actually a flying reptile, the Pteranodon is not a dinosaur.");
-      const pigeon = new Dino ("Pigeon", 0.5, 0.22, "Herbivore", "World Wide", "Holocene", "all birds are living dinosaurs")
 
-      let dinos = [dino1, dino2, dino3, dino4, dino5, dino6, dino7, pigeon]
+    const dinoInfo = [
+    {
+        "species": "Triceratops",
+        "weight": 13000,
+        "height": 114,
+        "diet": "herbivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "First discovered in 1889 by Othniel Charles Marsh"
+    },
+    {
+        "species": "Tyrannosaurus Rex",
+        "weight": 11905,
+        "height": 144,
+        "diet": "carnivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "The largest known skull measures in at 5 feet long."
+    },
+    {
+        "species": "Anklyosaurus",
+        "weight": 10500,
+        "height": 55,
+        "diet": "herbivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "Anklyosaurus survived for approximately 135 million years."
+    },
+    {
+        "species": "Brachiosaurus",
+        "weight": 70000,
+        "height": "372",
+        "diet": "herbivore",
+        "where": "North America",
+        "when": "Late Jurasic",
+        "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+    },
+    {
+        "species": "Stegosaurus",
+        "weight": 11600,
+        "height": 79,
+        "diet": "herbivore",
+        "where": "North America, Europe, Asia",
+        "when": "Late Jurasic to Early Cretaceous",
+        "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+    },
+    {
+        "species": "Elasmosaurus",
+        "weight": 16000,
+        "height": 59,
+        "diet": "carnivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+    },
+    {
+        "species": "Pteranodon",
+        "weight": 44,
+        "height": 20,
+        "diet": "carnivore",
+        "where": "North America",
+        "when": "Late Cretaceous",
+        "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+    },
+    {
+        "species": "Pigeon",
+        "weight": 0.5,
+        "height": 9,
+        "diet": "herbivore",
+        "where": "World Wide",
+        "when": "Holocene",
+        "fact": "All birds are living dinosaurs."
+    }];
 
-      //get original from json, make a new function that will assign constructor thru a loop?
+    const dinos = dinoInfo.map(dino => new Dino(dino.species, dino.weight, dino.height, dino.diet, dino.place, dino.time, dino.fact))
+
    
         // Create Human Object
 
         let human
-        let humancenter
  
         function Person (username,usercm,userkg,slctDiet) {
             this.name = 'human'
-            this.username = username
             this.height = usercm
             this.weight = userkg
             this.diet = slctDiet
+            this.displayName = username
         }    
         // Use IIFE to get human data from form
-    
-        const button = document.getElementById('btn')
-    
-        button.addEventListener("click", function () { 
-    
-        let username = document.getElementById('username').value;
-        let usercm = document.getElementById('usercm').value;
-        let userkg = document.getElementById('userkg').value;
-        let diet = document.getElementById('userdiet');
-        let slctDiet = diet.options[diet.selectedIndex].text;
 
-        return human = new Person (username,usercm,userkg,slctDiet)  
+        function fromForm () {
 
-    })
+            let username = document.getElementById('username').value;
+            let usercm = document.getElementById('usercm').value;
+            let userkg = document.getElementById('userkg').value;
+            let diet = document.getElementById('userdiet');
+            let slctDiet = diet.options[diet.selectedIndex].text;
+    
+            return human = new Person (username,usercm,userkg,slctDiet)
+        }
 
 
         // Create Dino Compare Method 1
@@ -93,8 +153,6 @@
       } else {console.log (`${randomDino.name}\'s diet is different than yours. He is a ${randomDino.diet}`)}
     };
 
-
-
     
         // Generate Tiles for each Dino in Array
 
@@ -112,41 +170,39 @@
             return arr;
           }
 
-          
+
         function TileGenerator() {
 
-
-            let humanDino = valueMiddleArray(shuffileArray(dinos), human);
+            valueMiddleArray(shuffleArray(dinos), human)
 
             const grid = document.getElementById("grid")
 
-            humanDinos.forEach(dino =>{
+            dinos.forEach(dino =>{
                 let tiles = document.createElement("div");
                 tiles.className = "grid-item"; 
-                var tileprops;
-                if(dino.name = 'human'){
-                    tileprops = `<h3>${dino.username}</h3><img src="images/${dino.name}.png" alt="image of ${dino.name}">`;
+                let tileprops;
+                if(dino.name == 'human'){
+                      tileprops = `<h3>${dino.displayName}</h3><img src="images/${(dino.name.toLowerCase())}.png" alt="image of ${dino.name}">`;
                 }
                 else{
-                    tileprops = `<h3>${dino.name}</h3><img src="images/${(dino.name.toLowerCase())}.png" alt="image of ${dino.name}"><p>${dino.fact}</p>`;
+                      tileprops = `<h3>${dino.species}</h3><img src="images/${(dino.species.toLowerCase())}.png" alt="image of ${dino.species}"><p>${dino.fact}</p>`
                 }
-                    tiles.innerHTML = tileprops;
-                    grid.appendChild(tiles)
-            });
-            }   
+                tiles.innerHTML = tileprops;
+                grid.appendChild(tiles)
+            })            }   
 
-            ///returns undefined dinos, study how foreach works and see what u can do with dino object
 
     
     // On button click, prepare and display infographic
+
+    const button = document.getElementById('btn')
     
-     button.addEventListener("click", function () {
+    button.addEventListener("click", function () {
        
         console.log("CLICK!!!");  
-   
 
-
-
-            }
-            )
+        fromForm();
+        TileGenerator();
+         });
+            
     
